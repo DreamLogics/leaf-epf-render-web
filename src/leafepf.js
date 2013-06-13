3f5d5c9017c4393d345a21359b3bc45bb6bb5239
Optima Developer <optima@optima-X79-UD5.(none)>
2013-06-13 17:57:09 +0200

    initial

---
+++ src/leafepf.js
@@ -0,0 +1,58 @@
+
+var EPFReader = {
+	loadFromURL: function(url)
+	{
+		
+	},
+	registerObjectType: function(type, factory)
+	{
+		
+	},
+    addPlatform: function(platform)
+    {
+    	
+    },
+    setLanguage: function(language)
+    {
+    	
+    },
+    addModule: function(module)
+    {
+    	
+    },
+    m_ObjectTypes: new Array(),
+    m_Platforms: new Array(),
+    m_sLanguage: "en",
+    m_Modules: new Array()
+};
+
+var EPFView = {
+	setView: function(view)
+	{
+		
+	},
+	setDocument: function(doc)
+	{
+		
+	},
+	setSection: function(section)
+	{
+		
+	},
+	nextSection: function()
+	{
+		
+	},
+	previousSection: function()
+	{
+		
+	},
+	tocSection: function()
+	{
+		
+	},
+	m_pView: 0,
+	m_pDocument: 0,
+	m_iCurrentSection: 0,
+	m_iTOCSection: -1
+};
